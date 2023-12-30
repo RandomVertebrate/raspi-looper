ba01bf821ec88fae342b07dba063e419fa2ea3d3
Riju Chatterjee <psychotenuse@gmail.com>
2023-12-30 13:13:36 -0500

    datatype

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ f.close()
 
 parameters[0] = input('Enter Sample Rate in Hz (Safe Choices 44100 and 48000): ') + '\n'
 parameters[1] = input('Enter Buffer Size (Typical 256, 512, 1024) : ') + '\n'
-parameters[2] = 50 #input('Enter Latency Correction in milliseconds: ') + '\n'
+parameters[2] = '50' #input('Enter Latency Correction in milliseconds: ') + '\n'
 parameters[3] = input('Enter Input Device Index (Probably 1 or 0) : ') + '\n'
 parameters[4] = input('Enter Output Device Index (Probably Same as Input) : ') + '\n'
 parameters[5] = input('Enter Margin for Late Button Press in Milliseconds (Around 500 seems to work well) : ') + '\n'
